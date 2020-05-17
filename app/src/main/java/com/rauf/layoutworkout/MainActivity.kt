148f3f2cb8e15b4b55c0845c0867416e9b72008a
rauf tababilov <tababilovrauf@gmail.com>
2020-05-17 06:31:37 +0500

    add Data Binding

--- app/src/main/java/com/rauf/layoutworkout/MainActivity.kt
+++ app/src/main/java/com/rauf/layoutworkout/MainActivity.kt
@@ -2,11 +2,29 @@ package com.rauf.layoutworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.rauf.layoutworkout.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        activityMainBinding.button.setOnClickListener{changeText()}
+
+        button.setOnClickListener{changeText()}
+    }
+
+    private fun changeText() {
+        val myTextView: TextView = findViewById(R.id.textView)
+
+        activityMainBinding.textView.text = "Data binding"
     }
 }
